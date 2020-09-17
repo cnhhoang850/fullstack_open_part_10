203110941ad1a64115f8e0c75171091943815624
Hoang Cao <cnhhoang850@gmail.com>
2020-09-17 10:20:12 +0700

    10a

--- rate-repo-app/App.js
+++ rate-repo-app/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Button } from 'react-native';
+import { StyleSheet, Text, ScrollView, Button } from 'react-native';
 
 export default function App() {
   return (
